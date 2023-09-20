07c49fb56ddaa6bc5acb6bad51249ee25d810ccc
o1ra <o1ra@yandex.ru>
2023-09-20 20:02:44 +0600

    fix conftest.py

--- conftest.py
+++ conftest.py
@@ -2,8 +2,10 @@ import pytest
 from selene import browser
 
 
-@pytest.fixture(scope='session', autouse=True)
+@pytest.fixture(scope='function', autouse=True)
 def browser_open():
     browser.config.window_width = 1920
     browser.config.window_height = 1080
     browser.open('https://google.com')
+    yield
+    browser.quit()
