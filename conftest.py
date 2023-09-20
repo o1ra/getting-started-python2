0701c0473d209e2ebe3187e2df37090c95414263
o1ra <o1ra@yandex.ru>
2023-09-20 18:52:55 +0600

    refactor code

--- conftest.py
+++ conftest.py
@@ -7,6 +7,3 @@ def browser_open():
     browser.config.window_width = 1920
     browser.config.window_height = 1080
     browser.open('https://google.com')
-
-
-
--- test_google_should_find_selene.py
+++ test_google_should_find_selene.py
@@ -1,10 +1,12 @@
 from selene.support.shared import browser
 from selene import be, have
 
+
 def test_search_yashaka():
     browser.element('[name="q"]').should(be.blank).type('yashaka/selene').press_enter()
     browser.element('[id="search"]').should(have.text('User-oriented Web UI browser tests in Python'))
+
+
 def test_no_results():
     browser.element('[name="q"]').clear().type('глрдроэошг98669700090').press_enter()
     browser.element('[id="result-stats"]').should(have.text('Результатов: примерно 0'))
-
